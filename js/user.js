70f5c36769c25991030feb114663b6d344c2dcc4
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-08-20 22:44:05 +0530

    user signup function implemented using firebase.

--- js/user.js
+++ js/user.js
@@ -1,14 +1,44 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCDhvqoZJi8hUfBOtgRZP8Z7zklJ60cdrk",
-  authDomain: "safe-share-d3d50.firebaseapp.com",
-  projectId: "safe-share-d3d50",
-  storageBucket: "safe-share-d3d50.appspot.com",
-  messagingSenderId: "631799901190",
-  appId: "1:631799901190:web:a5ddfb5d20a35944e5a787",
-};
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig); 
+$(document).ready(function () {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCDhvqoZJi8hUfBOtgRZP8Z7zklJ60cdrk",
+    authDomain: "safe-share-d3d50.firebaseapp.com",
+    databaseURL: "https://safe-share-d3d50-default-rtdb.firebaseio.com",
+    projectId: "safe-share-d3d50",
+    storageBucket: "safe-share-d3d50.appspot.com",
+    messagingSenderId: "631799901190",
+    appId: "1:631799901190:web:a5ddfb5d20a35944e5a787",
+  };
 
-//Set database variable
-var database = app.database()
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Set database variable
+  const db = getDatabase();
+
+  // Button click event
+  $("#signBtn").click(function () {
+    var email = $("#signEmail").val();
+    var password = $("#signPassword").val();
+
+    const uuid = crypto.randomUUID();
+
+    set(ref(db, "users/" + uuid), {
+      Email: email,
+      Password: password,
+    })
+      .then(() => {
+        alert("Saved");
+      })
+      .catch((error) => {
+        alert("Error");
+        console.log(error);
+      });
+  });
+});
