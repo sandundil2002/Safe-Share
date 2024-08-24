adfe3067481c53e18eb998fcd0765d311e460c45
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-08-24 10:58:45 +0530

    set the profile picture based on the username first letter.

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
   const storedUsername = localStorage.getItem("username");
-  const parts = storedUsername.split("@")
-  
+  const parts = storedUsername.split("@");
+
   $("#profileUsername").html(parts[0]);
-  
+
+  const firstLetter = parts[0].charAt(0).toUpperCase();
+
+  const imgSrc = `/img/${firstLetter}.png`;
+  $("#profilePic").attr("src", imgSrc);
+
   $("#signout-btn").click(function () {
-     window.location.href = "/index.html";
+    window.location.href = "/index.html";
   });
 });
